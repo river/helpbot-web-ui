9c1eadd77f53cf9bf970586a483f43c623d6b0f7
River Jiang <river@airfrost.ca>
2008-03-22 16:30:30 -0400

    Added tab link helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  def navi_tab_to(name, url)
+    link_to name, url, :class => (params[:controller] == name.downcase ? "current" : "background")
+  end
 end

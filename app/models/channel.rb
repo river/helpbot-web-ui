f9416c757d2df4e92b1e518fe9865082e051e364
River Jiang <river@airfrost.ca>
2008-03-22 16:36:50 -0400

    Fixed bug where you couldn't make a new channel without ticking both checkmarks.

--- app/models/channel.rb
+++ app/models/channel.rb
@@ -2,7 +2,7 @@ class Channel < ActiveRecord::Base
   has_many :triggers
   has_and_belongs_to_many :admins
   
-  validates_presence_of :name, :channel_wins, :public
+  validates_presence_of :name
   
   def admin_logins
     self.admins.each do |admin|

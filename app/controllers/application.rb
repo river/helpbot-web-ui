42dfce4ccc1c1c8af1921596079212f901f0af81
River Jiang <river@airfrost.ca>
2008-03-22 13:33:48 -0400

    initial commit

---
+++ app/controllers/application.rb
@@ -0,0 +1,10 @@
+# Filters added to this controller apply to all controllers in the application.
+# Likewise, all the methods added will be available for all controllers.
+
+class ApplicationController < ActionController::Base
+  helper :all # include all helpers, all the time
+
+  # See ActionController::RequestForgeryProtection for details
+  # Uncomment the :secret if you're not using the cookie session store
+  protect_from_forgery # :secret => '1c40c758eadd0c2ae72b2b71b6f65aa9'
+end

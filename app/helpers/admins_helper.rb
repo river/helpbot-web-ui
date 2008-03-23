4e1d6dcbfac7620772253902bcd343f7d4d4e3b0
Noah Lackstein <nlackstein@gmail.com>
2008-03-22 23:46:52 -0400

    Trying out some securing stuff... Admin time... Fix 3

--- app/helpers/admins_helper.rb
+++ app/helpers/admins_helper.rb
@@ -7,8 +7,6 @@ module AdminsHelper
   end
   
   def am_myself?
-    return if !logged_in?
-    
-    @current_admin.id == @admin.id || @current_admin.global
+    return if @current_admin.id != @admin.id && !@current_admin.global || !logged_in?
   end
 end

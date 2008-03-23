5609ff3c72f32ac94e024c01b9790a5ea8f58db2
Noah Lackstein <nlackstein@gmail.com>
2008-03-22 20:08:56 -0400

    Dang

--- app/helpers/admins_helper.rb
+++ app/helpers/admins_helper.rb
@@ -1,2 +1,8 @@
 module AdminsHelper
+
+  def admin_of
+    self.channels.each do |chan|
+      return chan.name
+    end
+  end
 end
--- app/helpers/channels_helper.rb
+++ app/helpers/channels_helper.rb
@@ -1,2 +1,8 @@
 module ChannelsHelper
+  
+  def admin_logins
+    self.admins.each do |admin|
+      return admin.login
+    end
+  end
 end

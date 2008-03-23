f719d359bfee3587da4721da6249a1faa26f2015
River Jiang <river@airfrost.ca>
2008-03-23 12:37:25 -0400

    Got rid of some superfluous triggers_controller actions and the "Triggers" tab in the layout.

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :admins
   map.resources :channels
   
+  # TODO: get rid of map.resources :triggers and add some more named routes for triggers
   map.trigger	'channels/:channel_id/triggers/:action/:id',
   				:controller => "triggers",
   				:action		=> "show",

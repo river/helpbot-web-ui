d7b6a4b9b241c98c932390bb4b8a05a601a24df9
Noah Lackstein <nlackstein@gmail.com>
2008-03-22 20:42:59 -0400

    Changed adminshelper a bit

--- app/helpers/admins_helper.rb
+++ app/helpers/admins_helper.rb
@@ -2,11 +2,7 @@ module AdminsHelper
 
   def admin_of
     @admin.channels.each do |chan|
-      if chan.empty?
-        puts "Nothing"
-      else
-        puts chan.name+" "
-      end
+      puts chan.name+" "
     end
   end
   

a64dd729251e4245e9015887d11fc3d96fff0c62
River Jiang <river@airfrost.ca>
2008-03-23 12:11:45 -0400

    Fixed 2 tiny bugs

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -9,7 +9,7 @@ class Admin < ActiveRecord::Base
   validates_length_of	  	:email,     :within => 3..100 
   validates_uniqueness_of	:email,	  	:case_sensitive => false 
   validates_uniqueness_of	:login,     :case_sensitive => false
-  validates_presence_of   :login, :email, :password
+  validates_presence_of   :login, :email
   validates_length_of	  	:password,	:within => 4..40, 
 										      :if =>      :password_required? 
   validates_confirmation_of :password,	:if => :password_required? 

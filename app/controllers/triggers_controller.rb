8d392753cd07be2a0d0a3a0bd58b080d547951a3
Noah Lackstein <nlackstein@gmail.com>
2008-03-23 12:18:37 -0400

    Cleaned up the triggers controller, and installed a pagination plugin (not used yet)

--- app/controllers/triggers_controller.rb
+++ app/controllers/triggers_controller.rb
@@ -1,65 +1,52 @@
 class TriggersController < ApplicationController
-  # GET /triggers
-  # GET /triggers.xml
+  before_filter :find_trigger_channel, :except => [:index, :new, :create]
+  before_filter :has_permission?, :except => [:index, :show]
+  
   def index
-    @triggers = Trigger.find(:all)
+    @triggers = Channel.find(params[:channel_id]).triggers
 
     respond_to do |format|
-      format.html # index.html.erb
+      format.html 
       format.xml  { render :xml => @triggers }
     end
   end
 
-  # GET /triggers/1
-  # GET /triggers/1.xml
   def show
-    @trigger = Trigger.find(params[:id])
 
     respond_to do |format|
-      format.html # show.html.erb
+      format.html
       format.xml  { render :xml => @trigger }
     end
   end
 
-  # GET /triggers/new
-  # GET /triggers/new.xml
-  #def new
-  #  @trigger = Trigger.new
-
-  #  respond_to do |format|
-  #    format.html # new.html.erb
-  #    format.xml  { render :xml => @trigger }
-  #  end
-  #end
-
-  # GET /triggers/1/edit
-  def edit
-    @trigger = Trigger.find(params[:id])
-    redirect_to(@trigger) unless owns_trigger(@trigger)
+  def new
+    @trigger = Trigger.new
+    
+    respond_to do |format|
+      format.html 
+      format.xml  { render :xml => @trigger }
+    end
   end
 
-  # POST /triggers
-  # POST /triggers.xml
-  #def create
-  #  @trigger = Trigger.new(params[:trigger])
+  def edit
+  end
 
-  #  respond_to do |format|
-  #    if @trigger.save
-  #      flash[:notice] = 'Trigger was successfully created.'
-  #      format.html { redirect_to(@trigger) }
-  #      format.xml  { render :xml => @trigger, :status => :created, :location => @trigger }
-  #    else
-  #      format.html { render :action => "new" }
-  #      format.xml  { render :xml => @trigger.errors, :status => :unprocessable_entity }
-  #    end
-  #  end
-  #end
+  def create
+    @trigger = Trigger.new(params[:trigger])
+    
+    respond_to do |format|
+      if @trigger.save
+        flash[:notice] = 'Trigger was successfully created.'
+        format.html { redirect_to(@trigger) }
+        format.xml  { render :xml => @trigger, :status => :created, :location => @trigger }
+      else
+        format.html { render :action => "new" }
+        format.xml  { render :xml => @trigger.errors, :status => :unprocessable_entity }
+      end
+    end
+  end
 
-  # PUT /triggers/1
-  # PUT /triggers/1.xml
   def update
-    @trigger = Trigger.find(params[:id])
-    redirect_to(@trigger) if !owns_trigger(@trigger)
 
     respond_to do |format|
       if @trigger.update_attributes(params[:trigger])
@@ -73,12 +60,7 @@ class TriggersController < ApplicationController
     end
   end
 
-  # DELETE /triggers/1
-  # DELETE /triggers/1.xml
   def destroy
-    @trigger = Trigger.find(params[:id])
-    return if !owns_trigger(@trigger)
-    
     @trigger.destroy
 
     respond_to do |format|
@@ -86,4 +68,14 @@ class TriggersController < ApplicationController
       format.xml  { head :ok }
     end
   end
+  
+  private
+    def find_trigger_channel
+      @trigger = Trigger.find_by_id(params[:id])
+      @channel = Channel.find_by_id(params[:channel_id])
+    end
+    
+    def has_permission?
+      owns_channel(@channel)
+    end
 end
